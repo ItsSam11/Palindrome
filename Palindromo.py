696333fa756a03d5f28843dd76d272158bc151f4
Marwan Samuel Flores Orozco <83468277+ItsSam11@users.noreply.github.com>
2022-12-07 22:11:13 -0600

    Update Palindromo.py

--- Palindromo.py
+++ Palindromo.py
@@ -1,10 +1,11 @@
 def saberPalindromo():
         nom = input('Ingrese una frase o una palabra para saber si es un palindromo: \n')
+        sep = nom
         nom = nom.replace(' ', '').lower()
         if nom[0::] == nom[::-1]:
-            print("'" + nom + "' " + 'Es un palindromo')
+            print("'" + sep + "' " + 'Es un palindromo')
         else: 
-            print("'" + nom + "' " + 'No es un palindromo')
+            print("'" + sep + "' " + 'No es un palindromo')
 
 
 if __name__ == '__main__':
